6ff75b232af247d9f49efd3098f4571561774480
Abieshek Subramaniam <abieshek.subramaniam@gmail.com>
2022-09-06 17:51:57 -0400

    aws changes

--- src/main/java/com/cognizant/domain/Customer/CustomerAddressWebController.java
+++ src/main/java/com/cognizant/domain/Customer/CustomerAddressWebController.java
@@ -20,7 +20,7 @@ import com.cognizant.domain.Address.Address;
 import com.cognizant.domain.Address.AddressRepository;
 
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "http://nb-customerservice.eba-4udtrze3.us-west-2.elasticbeanstalk.com/customers"})
 @RestController
 public class CustomerAddressWebController {
 	
